33cca3ced90d4483eefb4ff01c400ae22615d4d6
kingomes <omaressa13@aol.com>
2024-04-29 14:33:28 -0500

    part 3 progress

--- Q3.cpp
+++ Q3.cpp
@@ -11,6 +11,7 @@ condition_variable cv; // declare condition variable
 const int n = 5;
 const int m = 4;
 int counter = 0;
+int numListeners = n;
 
 
 struct requestStructure {
@@ -27,44 +28,62 @@ queue<requestStructure> msg_queue;
 string webPages[10] = {"google.com", "yahoo.com", "youtube.com", "amazon.com", "twitch.tv", "loyno.edu",
                         "totaljerkface.com", "spotify.com", "aol.com", "friv.com"};
 
-void listen() {
+void listen(int thread_id) {
 	srand(time(NULL));
+	
 	for (int i = 0; i < 6; i++) {
 		int timeToSleep = rand() % 3 + 1;
 	    this_thread::sleep_for(chrono::seconds(timeToSleep));
 	    {
+			unique_lock<mutex> lck(mtx);
 	    	counter++;
+			cout << "Listener: request no. " << counter << endl;
+			lck.unlock();
 	    	requestStructure request;
 		    request.request_id = counter;
 		    request.ip_address = "";
 		    int index = rand() % 10;
 		    request.page_requested = webPages[index];
+			lck.lock();
 		    msg_queue.push(request);
-		}
-	    
+			cout << "Listener: pushes request no. " << request.request_id << endl;
+			lck.unlock();
+		}  
     	cv.notify_one(); // notify waiting threads
 	}
+	numListeners--;
 }
-void do_request() {
-	int thread_id = 0;
+void do_request(int thread_id) {
 	while (true) {
 		unique_lock<mutex> lck(mtx);
 	    if (msg_queue.empty()) cv.wait(lck);
 	    int request_id = msg_queue.front().request_id;
+		cout << "Request: retrieving request number: " << request_id << endl;
 	    string page_requested = msg_queue.front().page_requested;
 	    msg_queue.pop();
-	    cout << "thread " << std::this_thread::get_id() << " completed request " << request_id << " requesting webpage " << page_requested << endl;
-	    thread_id++;
+	    cout << "thread " << thread_id << " completed request " << request_id << " requesting webpage " << page_requested << endl;
 	    lck.unlock();
-	    if (thread_id == 5) break;
+		cout << "Request: numListeners: " << numListeners << endl;
+		if (numListeners == 0 && msg_queue.empty()) {
+			cout << "Request: " << thread_id << " Exiting\n";
+			break;
+		}
 	}
 }
 
 int main() {
-	for (int i = 0; i < n; i++) {
-		thread t1(listen);
-		thread t2(do_request);
-		t1.join();
-		t2.join();
+	int k = n;
+	if (m>n) k = m;
+	thread listener[n];
+	thread requests[m];
+	for (int i = 0; i < k; i++) {
+		if (i<n)
+			listener[i] = thread(listen, i);
+		if (i<m)
+			requests[i] = thread(do_request, i);
+	}
+	for (int i = 0; i < k; i++) {
+		listener[i].join();
+		requests[i].join();
 	}
 }
