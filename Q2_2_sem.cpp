a511b23e3ab8e8a15b16ae3e892c8ba7aa3b2935
kingomes <83099848+kingomes@users.noreply.github.com>
2024-04-29 02:02:10 -0500

    progress in parts 2 and 3

--- Q2_2_sem.cpp
+++ Q2_2_sem.cpp
@@ -3,26 +3,29 @@
 #include <semaphore.h>
 using namespace std;
 
-sem_t sem;
+sem_t sem1;
+sem_t sem2;
 
 void runMeFirst() {
     cout << "Run me first" << endl;
-    sem_post(&sem);
+    sem_post(&sem1);
 }
 
 void runMeSecond() {
+	sem_wait(&sem1);
     cout << "I run second" << endl;
-    sem_post(&sem);
+    sem_post(&sem2);
 }
 
 void runMeThird() {
-    sem_wait(&sem);
+    sem_wait(&sem2);
     cout << "I run third" << endl;
 }
 
 int main() {
     // initialize the semaphore with the number of threads
-    sem_init(&sem, 0, -1); // 0: thread, 1: process
+    sem_init(&sem1, 0, 0); // 0: thread, 1: process
+    sem_init(&sem2, 0, 0); // 0: thread, 1: process
 
     thread t1 = thread(runMeFirst);
     thread t2 = thread(runMeSecond);
@@ -31,6 +34,7 @@ int main() {
     t2.join();
     t3.join();
     
-    sem_destroy(&sem); // destroy the semaphore
+    sem_destroy(&sem1); // destroy the semaphore
+    sem_destroy(&sem2); // destroy the semaphore
     return 0;
 }
--- Q3.cpp
+++ Q3.cpp
@@ -8,7 +8,10 @@ using namespace std;
 
 mutex mtx; // declare mutex
 condition_variable cv; // declare condition variable
-bool ready = false; // shared flag
+const int n = 5;
+const int m = 4;
+int counter = 0;
+
 
 struct requestStructure {
     int request_id; 
@@ -21,27 +24,47 @@ struct requestStructure {
 
 queue<requestStructure> msg_queue;
 
-string webPages[10] = {"google.com", "yahoo.com", "youtube.com", "amazon.com", "twitch.tv", "loyno.edu"
+string webPages[10] = {"google.com", "yahoo.com", "youtube.com", "amazon.com", "twitch.tv", "loyno.edu",
                         "totaljerkface.com", "spotify.com", "aol.com", "friv.com"};
 
 void listen() {
-    this_thread::sleep_for(chrono::seconds(3));
-    msg_queue.front().ip_address = "";
-
-    int index = rand() % 10;
-    msg_queue.front().page_requested = webPages[index];
-    ready = true;
-    cv.notify_one(); // notify waiting threads
+	srand(time(NULL));
+	for (int i = 0; i < 6; i++) {
+		int timeToSleep = rand() % 3 + 1;
+	    this_thread::sleep_for(chrono::seconds(timeToSleep));
+	    {
+	    	counter++;
+	    	requestStructure request;
+		    request.request_id = counter;
+		    request.ip_address = "";
+		    int index = rand() % 10;
+		    request.page_requested = webPages[index];
+		    msg_queue.push(request);
+		}
+	    
+    	cv.notify_one(); // notify waiting threads
+	}
 }
-
 void do_request() {
-    unique_lock<mutex> lck(mtx);
-    if (msg_queue.empty()) cv.wait(lck);
-
-    lck.unlock();
-    cout << "thread " << 1 << " completed request " << msg_queue.front().request_id << " requesting webpage " << msg_queue.front().page_requested << endl;
+	int thread_id = 0;
+	while (true) {
+		unique_lock<mutex> lck(mtx);
+	    if (msg_queue.empty()) cv.wait(lck);
+	    int request_id = msg_queue.front().request_id;
+	    string page_requested = msg_queue.front().page_requested;
+	    msg_queue.pop();
+	    cout << "thread " << std::this_thread::get_id() << " completed request " << request_id << " requesting webpage " << page_requested << endl;
+	    thread_id++;
+	    lck.unlock();
+	    if (thread_id == 5) break;
+	}
 }
 
 int main() {
-    do_request();
+	for (int i = 0; i < n; i++) {
+		thread t1(listen);
+		thread t2(do_request);
+		t1.join();
+		t2.join();
+	}
 }
